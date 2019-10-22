bc206f3e736588c1258312deadfe461a9a10cfef
Student <example@example.com>
2019-10-22 10:55:07 -0700

    edit css

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
   $("h1").click(function() {
     $("#text-1").toggle();
     $("#text-1").click(function() {
-      $("#img-1").toggle();
+      $("#img-1").fadeToggle();
       $("#img-1").click(function() {
-      $("#img-2").toggle();
+      $("#img-2").fadeToggle();
       $("#img-2").click(function() {
         $("#text-3").show();
         $("#text-3").click(function() {
-          $(".container").hide();
-          $("#Shrek").show();
+          $(".container").fadeOut().hide();
+          $("#Shrek").fadeIn().show();
       });
       });
     });
