75247b7d03aa17c4b0b7442a050765a8db10dc7c
Student <example@example.com>
2019-10-22 16:32:26 -0700

    add a sprinkle of confusion to our elegantly designed project

---
+++ js/scripts-jsComms.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  $("button#b1").click(function() {
+    $("ul#user").children("li").first().prepend("<li>Hello!</li>");
+    $("ul#webpage").children("li").first().prepend("ARG HOW DARE YOU SAY HELLO TO ME, do I not look busy drowning?");
+     $('li').click(function() {
+
+    $("ul#user").children("li").first().click(function() {
+      alert('hi');
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      alert('hi');
+    });
+  });
+    alert("GLAAAHHHHH GOOOLAAAAA HELP I'M DROWNING YOU HAVE TO CLICK ME AGSINSFajkdhkhrgk");
+  });
+    $("ul#webpage").children("li").first().prepend("<li>Why hello there!</li>");
+     $('li').click(function() {
+      alert("Oh, nevermind. I'm okay after all.");
+    });
+  });
+
+
+
+
+  $("button#b2").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+
+
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+     $('li').click(function() {
+  });
+
+  $("button#b3").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+
+
+    alert("NO GASTON!");
+
+
+
+
+
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+  });
+});
